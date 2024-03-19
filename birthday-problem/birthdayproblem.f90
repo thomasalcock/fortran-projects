56182988232523e49324c5ca5c404123bf682213
thomasalcock <thomas.alcock@statworx.com>
2024-03-19 20:46:51 +0100

    add format specifier

--- birthday-problem/birthdayproblem.f90
+++ birthday-problem/birthdayproblem.f90
@@ -5,7 +5,7 @@ program birthdayproblem
     integer, dimension(50) :: n_people = [(j, j = 5, 54)]
     integer :: i
     do i = 1, size(n_people)
-        print *, i, ",", simulate_birthday_problem(n_people(i))
+        print "(I2,A,F10.8)", i, ",", simulate_birthday_problem(n_people(i))
     end do
 
 end program
